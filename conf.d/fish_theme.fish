d84109525a586431cda2c74b1ec91a7d67a5226a
Rulfam <77755588+Rulfam@users.noreply.github.com>
2026-01-04 02:26:18 +0200

    Update to v4.3

---
+++ conf.d/fish_theme.fish
@@ -0,0 +1,39 @@
+#!/usr/bin/env fish
+
+set --global fish_color_autosuggestion brblack
+set --global fish_color_cancel --reverse
+set --global fish_color_command normal
+set --global fish_color_comment brblack --italics
+set --global fish_color_cwd cyan
+set --global fish_color_cwd_root cyan --bold
+set --global fish_color_end brmagenta
+set --global fish_color_error brred
+set --global fish_color_escape brcyan
+set --global fish_color_history_current --bold
+set --global fish_color_host yellow
+set --global fish_color_host_remote brmagenta
+set --global fish_color_keyword normal
+set --global fish_color_normal normal
+set --global fish_color_operator brcyan
+set --global fish_color_option cyan
+set --global fish_color_param cyan
+set --global fish_color_quote green
+set --global fish_color_redirection cyan --bold
+set --global fish_color_search_match white --bold --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user green
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_background
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description yellow --italics
+set --global fish_pager_color_prefix normal --bold --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --reverse
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
